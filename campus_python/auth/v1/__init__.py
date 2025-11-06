d9e6b4c9cf950064f4fda294d6fc6cc7751d1e75
JS Ng <ngjunsiang@gmail.com>
2025-11-06 04:56:42 +0000

    refactor: Resource and ResourceCollection fallback to parent/root client if not set

--- campus_python/auth/v1/__init__.py
+++ campus_python/auth/v1/__init__.py
@@ -20,7 +20,7 @@ class AuthRoot(ResourceRoot):
     path: str = "/auth/v1"
 
     def __init__(self, json_client: JsonClient):
-        self._json_client = json_client
+        super().__init__(json_client=json_client)
         self._clients = None
         self._credentials = None
         self._logins = None
@@ -32,58 +32,40 @@ class AuthRoot(ResourceRoot):
     def clients(self) -> clients.Clients:
         """Get the clients resource."""
         if not self._clients:
-            self._clients = clients.Clients(
-                self._json_client,
-                root=self
-            )
+            self._clients = clients.Clients(root=self)
         return self._clients
 
     @property
     def credentials(self) -> credentials.Credentials:
         """Get the credentials resource."""
         if not self._credentials:
-            self._credentials = credentials.Credentials(
-                self._json_client,
-                root=self
-            )
+            self._credentials = credentials.Credentials(root=self)
         return self._credentials
 
     @property
     def logins(self) -> logins.Logins:
         """Get the logins resource."""
         if not self._logins:
-            self._logins = logins.Logins(
-                self._json_client,
-                root=self
-            )
+            self._logins = logins.Logins(root=self)
         return self._logins
 
     @property
     def sessions(self) -> sessions.CampusSessions:
         """Get the sessions resource."""
         if not self._sessions:
-            self._sessions = sessions.CampusSessions(
-                self._json_client,
-                root=self
-            )
+            self._sessions = sessions.CampusSessions(root=self)
         return self._sessions
 
     @property
     def users(self) -> users.Users:
         """Get the users resource."""
         if not self._users:
-            self._users = users.Users(
-                self._json_client,
-                root=self
-            )
+            self._users = users.Users(root=self)
         return self._users
 
     @property
     def vaults(self) -> vaults.Vaults:
         """Get the vaults resource."""
         if not self._vaults:
-            self._vaults = vaults.Vaults(
-                self._json_client,
-                root=self
-            )
+            self._vaults = vaults.Vaults(root=self)
         return self._vaults
